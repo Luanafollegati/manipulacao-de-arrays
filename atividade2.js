8dc014200b49d4c3868e083995824a4d7b5e4267
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 11:59:41 -0300

    feat: Adiciona novos produtos ao cardapio

--- atividade2.js
+++ atividade2.js
@@ -1,1 +1,11 @@
 // 02
+let cardapio = ["pizza de calabresa", "lasanha", "sushi", "salada Caesar", "pastel", "yakisoba", "feijoada"];
+  
+console.log("antes do push");
+console.table(cardapio);
+
+// adicionando novos produtos ao cardapio 
+cardapio.push("hambúrguer artesanal", "crap de frango");
+
+console.log("depois do push");
+console.table(cardapio);
