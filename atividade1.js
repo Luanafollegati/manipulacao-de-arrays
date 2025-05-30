9cce20805901eebbd01e5cdda1a5852be52d077a
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-05-30 11:54:35 -0300

    feat: Adiciona novos produtos

--- atividade1.js
+++ atividade1.js
@@ -1,1 +1,11 @@
 // 01
+let promocoes = ["teclado mecânico", "mouse gamer", "monitor ultrawide", "cadeira ergonômica",  "webcam HD", "fone bluetooth", "notebook i5", "hub USB"];
+
+console.log("antes do push");
+console.table(promocoes);
+
+// adicionando novos produtos em promoção 
+promocoes.push("SSD 1TB", "mesa digitalizadora");
+
+console.log("depois do push");
+console.table(promocoes);
