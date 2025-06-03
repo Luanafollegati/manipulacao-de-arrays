a67927fb669c2dc7c66f64869ebd1d03bbdbf9f6
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:15:44 -0300

    feat: remova o ultimo inscrito

--- atividade4.js
+++ atividade4.js
@@ -1,1 +1,13 @@
 //04
+let inscritos = ["Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"];
+
+console.log("antes do pop");
+console.table(inscritos);
+
+// removendo o ultimo inscrito
+let inscritoRemovido = inscritos.pop();
+
+console.log("depois do pop");
+console.table(inscritos);  
+
+console.log("Inscrito removido: ", inscritoRemovido);
