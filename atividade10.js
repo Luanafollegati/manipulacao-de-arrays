bda980552ed91814fc17d431d671ab293dc68308
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:49:21 -0300

    feat:  Verifica se item esta disponivel

--- atividade10.js
+++ atividade10.js
@@ -1,1 +1,5 @@
 //10
+let estoque = ["mesa para PC", "cadeira gamer", "monitor 27 polegadas", "hub USB", "mouse sem fio"];
+
+// Verificando se item esta disponivel em estoque
+console.log(`Ainda temos cadeira gamer em estoque? ${estoque.includes("cadeira gamer")} `);
