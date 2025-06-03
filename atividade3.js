59763ebd466fd1d443b549cc0ae9761539d5d9d6
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:11:30 -0300

    feat: remove o ultimo item da lista

--- atividade3.js
+++ atividade3.js
@@ -1,1 +1,13 @@
 // 03
+let producao = ["mousepad", "placa de vídeo", "fonte de alimentação", "HD externo", "notebook gamer", "roteador"];
+
+console.log("antes do pop");
+console.table(producao);
+
+// removendo o ultimo item
+let itemRemovido = producao.pop();
+
+console.log("depois do pop");
+console.table(producao);  
+
+console.log("Item removido: ", itemRemovido);
