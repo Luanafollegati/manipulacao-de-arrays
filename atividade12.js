0eeb5ceea263e777e3d1bcad979152661dce28be
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 23:19:01 -0300

    feat:Converta o array de palavras-chave em uma string

--- atividade12.js
+++ atividade12.js
@@ -1,1 +1,5 @@
 //12
+let palavrasChave = ["desenvolvimento", "frontend", "javascript", "css", "react"];
+
+// Convertendo o array de palavras-chave em uma string separada por traços
+console.log ("As palavras-chaves são:" , palavrasChave.join("- "));
