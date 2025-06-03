cd9aad8b0698bf1a84ed4e50ffc7a02d9c11d270
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:26:17 -0300

    feat: Remova a primeira mensagem

--- atividade6.js
+++ atividade6.js
@@ -1,1 +1,13 @@
 //06
+let mensagens = ["Propaganda indevida", "Bom dia, posso ajudar?", "Preciso de suporte técnico", "Claro, qual o problema?", "Erro ao acessar conta"];
+
+console.log("antes do shift");
+console.table(mensagens);
+
+// Removendo a primeira mensagem
+let mensagemRemovida = mensagens.shift();
+
+console.log("depois do shift");
+console.table(mensagens);
+
+console.log("Mensagem Removida: ", mensagemRemovida);
