b40d5a8d3c6d3d621f4e9912e71812fe830f0126
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:35:02 -0300

    feat: Adiciona alunas atrasadas no inicio da fila

--- atividade8.js
+++ atividade8.js
@@ -1,1 +1,12 @@
 //08
+let chamada = ["Carlos", "Fernanda", "João", "Mariana", "Lucas", "Camila"];
+
+console.log("antes do unshift");
+console.table(chamada);
+
+//Adicionando alunas atrasadas no inicio da fila
+chamada.unshift("Beatriz")
+chamada.unshift("Rafaela")
+
+console.log("depois do unshift");
+console.table(chamada);
