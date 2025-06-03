090a64c7f34ba6a28265a567668094964f73879f
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:57:53 -0300

    feat: exiba ingredientes de uma receita em uma única frase

--- atividade11.js
+++ atividade11.js
@@ -1,1 +1,5 @@
 //11
+let ingredientes = ["farinha de trigo", "fermento", "leite", "ovos", "açúcar", "manteiga"];
+
+// exibindo os ingredientes de uma receita em uma única frase 
+console.log("Ingredientes:", ingredientes.join(", "));
