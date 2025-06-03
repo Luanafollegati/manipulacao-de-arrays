2e1333c1d7a779ab13c07b9a87883d1f53496284
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:44:34 -0300

    feat: Verifica presença na lista

--- atividade9.js
+++ atividade9.js
@@ -1,1 +1,5 @@
 //09
+let presenca = ["João Lima", "Carla Nunes", "Amanda Souza", "Bruno Ribeiro", "Fernanda Sales"];
+
+// Verificando presença na lista
+console.log(`"Amanda Souza" compareceu à reunião de alinhamento? ${presenca.includes("Amanda Souza")} `);
