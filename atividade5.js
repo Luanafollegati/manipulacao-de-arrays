926f0426109ee892346422248d29b948d7feec7d
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:20:42 -0300

    feat: Remova o primeiro item

--- atividade5.js
+++ atividade5.js
@@ -1,1 +1,13 @@
 //05
+let importados = ["carregador portátil", "cabo HDMI", "leitor biométrico", "controle sem fio", "webcam 4K"];
+
+console.log("antes do shift");
+console.table(importados);
+
+// Removendo o primeiro item
+let ItemRemovido = importados.shift();
+
+console.log("depois do shift");
+console.table(importados);
+
+console.log("Item Removido: ", ItemRemovido);
