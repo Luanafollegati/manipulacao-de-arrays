0930415accc44a52157f97d7b6c2a74a78f2bce5
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-02 22:30:51 -0300

    feat: Adiciona novos clientes no inicio da fila

--- atividade7.js
+++ atividade7.js
@@ -1,1 +1,12 @@
 //07
+let filaAtendimento = ["cliente 003", "cliente 004", "cliente 005", "cliente 006", "cliente 007"];
+
+console.log("antes do unshift");
+console.table(filaAtendimento);
+
+//Adicionando novos clientes no inicio da fila
+filaAtendimento.unshift("cliente VIP 1")
+filaAtendimento.unshift("cliente VIP 2")
+
+console.log("depois do unshift");
+console.table(filaAtendimento);
