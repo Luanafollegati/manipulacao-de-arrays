3ebac1f8bb90d623703ad35e56f3b7a195c961d2
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-10 17:18:55 -0300

    feat: Junta as duas listas de usuarios

--- atividade17.js
+++ atividade17.js
@@ -3,5 +3,5 @@ let usuariosSistemaA = ["aline.souza", "maria.ferreira", "carlos.lima", "ana.mar
   
 let usuariosSistemaB = ["joao.silva", "paulo.pereira", "fernanda.alves", "marcelo.cardoso"];
   
-// Juntado as duas listas de usuarios
+// Juntado as duas listas de usuario
 console.log(usuariosSistemaA.concat(usuariosSistemaB));
