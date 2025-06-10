d3150078a81e9c19a1ce863416a29a2525e30c14
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-10 17:17:53 -0300

    feat: Junta as duas listas de itens

--- atividade17.js
+++ atividade17.js
@@ -1,8 +1,7 @@
 // 17
 let usuariosSistemaA = ["aline.souza", "maria.ferreira", "carlos.lima", "ana.martins"];
   
-  let usuariosSistemaB = ["joao.silva", "paulo.pereira", "fernanda.alves", "marcelo.cardoso"];
+let usuariosSistemaB = ["joao.silva", "paulo.pereira", "fernanda.alves", "marcelo.cardoso"];
   
-// Juntado as duas listas de usuários
-
+// Juntado as duas listas de usuarios
 console.log(usuariosSistemaA.concat(usuariosSistemaB));
--- atividade18.js
+++ atividade18.js
@@ -1,1 +1,8 @@
 // 18
+let hardware = [ "processador i7", "placa-mãe", "memória RAM 16GB", "SSD 1TB"];
+
+let perifericos = ["teclado gamer", "mouse ótico", "webcam HD", "fone com microfone"];
+
+// Juntado as duas listas de itens
+
+console.log(hardware.concat(perifericos));
