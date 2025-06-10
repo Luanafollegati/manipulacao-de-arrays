e674dc4abf4da1cddde58f6a62dbab55ac0b50fd
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-10 17:08:24 -0300

    feat: Substituindo matérias na grade

--- atividade16.js
+++ atividade16.js
@@ -1,1 +1,9 @@
 // 16
+let gradeCurricular = ["Matemática", "Português", "Biologia", "Física", "História", "Educação Física", "Inglês"];
+  
+  console.table(gradeCurricular);
+
+// Removendo 3 elementos a partir do índice 4
+  gradeCurricular.splice(2, 3, "Programação" , "Robotica")
+  
+  console.table(gradeCurricular);
