4cd12a0831b613c838614d830fcaf3890e80f46c
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-10 17:42:23 -0300

    feat: Organiza os candidatos em ordem alfabética

--- atividade23.js
+++ atividade23.js
@@ -1,1 +1,8 @@
 // 23
+let candidatos = ["Marcos", "Aline", "Carla", "João", "Fernanda", "Lucas", "Bruno", "Isabela"];
+
+  
+// Organizando os candidatos em ordem alfabética
+let conditatosOrdenados = candidatos.sort();
+
+console.log("Candidatos ordenados:", conditatosOrdenados);
