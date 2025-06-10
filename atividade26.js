ae7f14b37bcc7378fa40167d7180253818b2a046
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-10 17:49:30 -0300

    feat: Inverta o histórico

--- atividade26.js
+++ atividade26.js
@@ -1,1 +1,7 @@
 // 26
+
+let historico = [ "login", "painel", "relatórios", "detalhes do cliente", "editar dados", "configurações", "logout"];
+
+// Invertendo histórico
+  let historicoInvertido = historico.reverse()
+  console.log(historicoInvertido);
