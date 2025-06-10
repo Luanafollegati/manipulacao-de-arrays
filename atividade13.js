2a8b3a259e97780444ccb94ec2da10d689c0a1c9
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-10 16:17:30 -0300

    Feat: Cria arquivo atividade14.js

--- atividade13.js
+++ atividade13.js
@@ -1,1 +1,5 @@
 // 13
+let diasFaltas = ["Carlos – Segunda", "Carlos – Terça","Carlos – Quarta","Carlos – Quinta", "Carlos – Sexta", "Carlos – Segunda (semana 2)", "Carlos – Terça (semana 2)", "Carlos – Quarta (semana 2)"];
+
+// Relatório com apenas a primeira semana de férias
+console.log("relatorio da semana:", diasFaltas.slice(0, 5));
