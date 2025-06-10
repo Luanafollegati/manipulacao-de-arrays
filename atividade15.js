6dc2b6a2ffe49903761c65d67b400ff01729c3c0
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-06-10 17:03:03 -0300

    feat:  Remova 2 elementos

--- atividade15.js
+++ atividade15.js
@@ -1,1 +1,9 @@
 // 15
+let menuNavegacao = ["Início", "Serviços", "Portfólio", "Blog", "Contato", "Login", "Área do cliente"];
+
+// Removendo 2 elementos
+let elementosRemovidos = menuNavegacao.splice(1, 2);
+
+console.table(menuNavegacao);
+
+console.log("Elementos removidos:", elementosRemovidos);
